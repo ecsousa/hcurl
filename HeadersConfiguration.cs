24ab357b9c5430805c341001d5c4915aa3719ab0
Eduardo Sousa <ecsousa@gmail.com>
2023-08-10 11:01:17 -0700

    Initial commit

---
+++ HeadersConfiguration.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace hcurl
+{
+    class HeadersConfiguration {
+
+        private readonly List<(Regex pattern, List<(string key, string value)> headers)> headersByHostPattern;
+
+        public HeadersConfiguration(string headersFile) {
+
+            this.headersByHostPattern = new List<(Regex pattern, List<(string key, string value)> headers)>();
+
+            var headerRegex = new Regex(@"^\s+(?<key>[\w-]+)\s*:\s*(?<value>\S.*\S)\s*$", RegexOptions.Compiled);
+            List<(string key, string value)>? currentList = null;
+
+            foreach (var line in File.ReadAllLines(headersFile)) {
+                var match = headerRegex.Match(line);
+
+                if (match.Success) {
+
+                    var header = (key: match.Groups["key"].Value, value: match.Groups["value"].Value);
+
+                    if (currentList == null) {
+                        throw new Exception($"No host pattern found for header {header.key}: {header.value}");
+                    }
+
+                    currentList.Add(header);
+                }
+
+                else if (!string.IsNullOrWhiteSpace(line)) {
+                    currentList = new List<(string key, string value)>();
+
+                    this.headersByHostPattern.Add((new Regex($"^({line.Trim()})$"), currentList));
+                }
+
+            }
+
+        }
+
+        public (string key, string value)[] GetHeaders(Uri url) {
+            return this.headersByHostPattern
+                .Where(hostHeaders => hostHeaders.pattern.IsMatch(url.Host))
+                .SelectMany(hostHeaders => hostHeaders.headers)
+                .ToArray();
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using hcurl;
+using System.Diagnostics;
+
+var headerFile = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".headers");
+
+var url = args
+    .Select(a => Uri.TryCreate(a, UriKind.Absolute, out var url) ? url : null)
+    .FirstOrDefault(a => a != null);
+
+var psi = new ProcessStartInfo();
+
+if (url != null && File.Exists(headerFile))
+{
+    var headerList = new HeadersConfiguration(headerFile).GetHeaders(url);
+
+    foreach(var header in headerList)
+    {
+        psi.ArgumentList.Add("-H");
+        psi.ArgumentList.Add($"{header.key}: {header.value}");
+    }
+
+}
+
+foreach (var item in args)
+    psi.ArgumentList.Add(item);
+
+psi.FileName = "curl.exe";
+psi.UseShellExecute = false;
+
+var process = Process.Start(psi);
+
+if(process == null)
+{
+    Environment.Exit(1);
+}
+
+process.WaitForExit();
+
+Environment.Exit(process.ExitCode);
